d19b3c6ec92b39296b2a10777365536e39b323e5
zahitucman <zahitucmann@gmail.com>
2023-08-17 19:14:13 +0300

    self code review

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -4,25 +4,26 @@ class GithubService
 
   def get_repositories(params)
     search_term = params[:search_term]
+
+    if !search_term
+      return []
+    end
+
     sort_column = params[:sort_column] || 'stars'
     sort_direction = params[:sort_direction] || 'desc'
     page = params[:page] || 1
     per_page = params[:per_page] || 10
-    
-    if search_term
-      begin
-        response = RestClient.get(
-          "https://api.github.com/search/repositories?q=#{search_term}&sort=#{sort_column}&order=#{sort_direction}&page=#{page}&per_page=#{per_page}",
-          { 'X-GitHub-Api-Version': "2022-11-28"}
-        )
 
-        repositories = JSON.parse(response)['items']
-      rescue RestClient::ExceptionWithResponse => e
-        puts "Response Code: #{e.response.code}"
-        puts "Response Body: #{e.response.body}"
-      end
-    else
-      repositories = []
+    begin
+      response = RestClient.get(
+        "https://api.github.com/search/repositories?q=#{search_term}&sort=#{sort_column}&order=#{sort_direction}&page=#{page}&per_page=#{per_page}",
+        { 'X-GitHub-Api-Version': "2022-11-28"}
+      )
+
+      repositories = JSON.parse(response)['items']
+    rescue RestClient::ExceptionWithResponse => e
+      puts "Response Code: #{e.response.code}"
+      puts "Response Body: #{e.response.body}"
     end
 
     repositories
